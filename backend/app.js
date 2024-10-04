8df799ebc541ec0dd045a3812f9756932d9ee56d
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-10-04 11:28:09 +0900

    Backend default settings /api

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* const cors = require('cors'); */
+/* var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users'); */
+const apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -13,14 +14,40 @@ var app = express();
 /* app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); */
 
+
+
+/* 
+// connect to mongodb
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://kwagi:q4VQtadoiPgXgqBo@cluster0.s1ckl.mongodb.net/kmla_storage?retryWrites=true&w=majority&appName=Cluster0";
+
+// Get the default connection
+const db = mongoose.connection;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+ */
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/* app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  
+})) */
+
+
+/* app.use('/', indexRouter);
+app.use('/users', usersRouter); */
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// API Index
+router.get('/', (req, res) => {
+  res.json({ message: 'API Index' });
+});
+
+/* router.use("/item", itemRouter); */
+
+module.exports = router;
