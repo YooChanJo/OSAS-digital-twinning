2482eb48e866d8da62ae443171f5e1d0ed4dbf71
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-10-04 13:41:50 +0900

    Added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>Root</>
+  },
+]);
 
 function App() {
-
-  return <>Hello App</>;
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
