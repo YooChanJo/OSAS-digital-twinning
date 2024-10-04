8b0662965fae1b9ac4f1964a3e5d50bd8b3fd866
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-10-04 13:48:10 +0900

    Added test playground for human gas change calculation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HumanGasExchange from './routes/humanGasExchange';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <>Root</>
+  },
+  {
+    path: "/human/gas-exchange", // need to change path name to a more better one
+    element: <HumanGasExchange />
   },
 ]);
 
---
+++ frontend/src/routes/humanGasExchange.jsx
@@ -0,0 +1,5 @@
+
+export default function HumanGasExchange() {
+
+    return <>Hi</>
+}
