7dab8ad1cec68dc6f5b9b5e7849452f0e784383b
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-10-20 21:57:25 +0900

    Added input

--- frontend/src/routes/bloodO2PressureAnalysis.jsx
+++ frontend/src/routes/bloodO2PressureAnalysis.jsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
+
 
 export default function BloodO2PressureAnalysis() {
     const navigate = useNavigate();
     const onBackToHomeClick = () => {
         navigate("/");
     }
+    const onFormSubmit = () => {
+
+    }
+    
     return (<>
-
-
-    <button onClick={onBackToHomeClick}>Back to Home</button>
+        <h2>Welcome to blood O2 pressure analysis page</h2>
+        <form onSubmit={onFormSubmit} style={{border: "1px solid black", padding: "10px", marginBottom: "10px"}}>
+            <h2>Inital Conditions</h2>
+            <label htmlFor="Pv">O2 pressure of the venous blood (mmHg)</label><br />
+            <input id="Pv" type="number" defaultValue={40}></input><br />
+            
+            <label htmlFor="Vr">Volume of residue (L)</label><br />
+            <input id="Vr" type="number" defaultValue={1}></input><br />
+            <label htmlFor="Pr">Inital O2 pressure of residue (mmHg) - (cyle 0)</label><br />
+            <input id="Pr" type="number" defaultValue={100}></input>
+            <h2>Predefined Constants</h2>
+            <p>
+                Xo2 = 19.7%, A = 8 * 10^7 mm^2, L = 1mm, v = 1.33 mm/s, <br/>
+                T = 310K, R = 62.3637 mmHg * L / (mol * K)
+            </p>
+        </form>
+        <button onClick={onBackToHomeClick}>Back to Home</button>
     </>);
 }
