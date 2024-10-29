84e34ebae214c3d1614b99bc2d77a4435346dc00
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-10-29 16:06:00 +0900

    Added precision element to frontend form

--- frontend/src/routes/bloodO2PressureAnalysis.jsx
+++ frontend/src/routes/bloodO2PressureAnalysis.jsx
@@ -13,8 +13,9 @@ export default function BloodO2PressureAnalysis() {
         const Part = Number(e.target[1].value);
         const Vr = Number(e.target[2].value);
         const Pr = Number(e.target[3].value);
+        const n = Number(e.target[4].value); // precision
         try {
-            const res = await axios.post("/api/blood-pressure-analysis", { Pv, Part, Vr, Pr });
+            const res = await axios.post("/api/blood-pressure-analysis", { Pv, Part, Vr, Pr, n });
             console.log(res);
         } catch (err) {
             console.log(err.message);
@@ -26,13 +27,15 @@ export default function BloodO2PressureAnalysis() {
         <form onSubmit={onFormSubmit} style={{border: "1px solid black", padding: "10px", marginBottom: "10px"}}>
             <h2>Inital Conditions</h2>
             <label htmlFor="Pv">O2 pressure of the venous blood (mmHg)</label><br />
-            <input id="Pv" type="number" defaultValue={40}></input><br />
+            <input id="Pv" type="number" defaultValue={40} required></input><br />
             <label htmlFor="Part">Initial O2 pressure of the arterial blood (mmHg) - (cycle 0)</label><br />
-            <input id="Part" type="number" defaultValue={100}></input><br />
+            <input id="Part" type="number" defaultValue={100} required></input><br />
             <label htmlFor="Vr">Volume of residue (L)</label><br />
-            <input id="Vr" type="number" defaultValue={1}></input><br />
+            <input id="Vr" type="number" defaultValue={1} required></input><br />
             <label htmlFor="Pr">Inital O2 pressure of residue (mmHg) - (cycle 0)</label><br />
-            <input id="Pr" type="number" defaultValue={100}></input>
+            <input id="Pr" type="number" defaultValue={100} required></input><br />
+            <label htmlFor="resolution">Number of segments (resolution)</label><br />
+            <input id="resolution" type="number" defaultValue={10000} required></input>
             <h2>Predefined Constants</h2>
             <p>
                 Xo2 = 19.7%, A = 8 * 10^7 mm^2, L = 1mm, v = 1.33 mm/s, <br/>
