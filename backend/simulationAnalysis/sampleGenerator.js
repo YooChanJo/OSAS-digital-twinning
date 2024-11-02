60ebd82584745f03d64d3f8f8ac20649e5a3366b
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-11-02 17:58:41 +0900

    Fixed samplegenrator to create samples of multiple cycles

--- backend/simulationAnalysis/sampleGenerator.js
+++ backend/simulationAnalysis/sampleGenerator.js
@@ -4,26 +4,28 @@ const period = 5;
 const inhalation = 2;
 const exhalation = period - inhalation;
 
-const VinSampleFunction = (t) => { // 0 <= t <= period
+const VinSampleFunction = (t) => {
+    t -= Math.floor(t / period) * period
     t *= PI / ( 2 * period );
     return Math.sqrt(3) * 3 / 4  * Math.sin(t) * Math.cos(t) * Math.cos(t);
 }
 const PinSampleFunction = (t) => {
+    t -= Math.floor(t / period) * period
     t *= PI / (2 * period);
     if( t <= inhalation ) return -Math.sin(PI * t / inhalation);
     else return Math.sin(PI * (t-inhalation) / exhalation); // Is not going to be needed
 }
 
-const VinSampleGenerator = (dt) => {
+const VinSampleGenerator = (cycles, dt) => {
     const result = [];
-    for(let t = 0; t <= period; t += dt) {
+    for(let t = 0; t <= period * cycles; t += dt) {
         result.push(VinSampleFunction(t));
     }
     return result;
 }
-const PinSampleGenerator = (dt) => {
+const PinSampleGenerator = (cycles, dt) => {
     const result = [];
-    for(let t = 0; t <= period; t += dt) {
+    for(let t = 0; t <= period * cycles; t += dt) {
         result.push(PinSampleFunction(t));
     }
     return result;
--- backend/simulationAnalysis/twin.js
+++ backend/simulationAnalysis/twin.js
@@ -1,17 +1,17 @@
 const defaultPredefines = require("./defaultPredefines");
 const sampleGenerator = require("./sampleGenerator");
 
-class Twin {
+class Twin { // has no idea of cycles
     /* 
-        cycleNumber, t
+        t
         predefines { Xo2, A, L, v, T, R, SaO2, CoO2 }
         na
         Va, Pa
         resolution, dt
         Pb = [   ] // size resolution + 1 | meaningful data from [1]
         input {
-            Vin = [] // sample interval of dt
-            Pin = []
+            Vin = [] // sample interval of dt // should start with 0 end with 0
+            Pin = [] // should start with 760mmHg and end with 760mmHg
             *optional size = (number) // Vin Pin size
         }
     */
@@ -27,8 +27,7 @@ class Twin {
         if(!predefines) this.predefines = defaultPredefines; // predefines 정의 안됨
         else this.predefines = predefines;
         // properties
-        this.cycleNumber = 0;
-        this.t = 0; // 0 <= t <= 5
+        this.t = 0;
 
         this.na = (initialConditions.Pr * initialConditions.Vr) / (this.predefines.R * this.predefines.T);
         this.Va = initialConditions.Vr;
@@ -46,12 +45,8 @@ class Twin {
             Pin: null,
         }
     }
-    updatePa = () => { 
-        this.Pa = (this.na * this.predefines.R * this.predefines.T) / (this.Va);
-    }
-    updatedt = () => { 
-        this.dt = (this.predefines.L) / (this.resolution * this.predefines.v);
-    }
+    updatePa = () => { this.Pa = (this.na * this.predefines.R * this.predefines.T) / (this.Va); }
+    updatedt = () => { this.dt = (this.predefines.L) / (this.resolution * this.predefines.v); }
     getPart = () => this.Pb[this.resolution];
     
     // nomalizeGeneralInput
@@ -64,9 +59,20 @@ class Twin {
         this.input.Pin = null;
     }
     
-    evolveCycle() {
-        
+    evolveCycle(callback) { // boolean | success ? True : False
+        if(!callback) callback = () => {};
+        // t = 0
+        this.t = 0;
+        this.Va += this.input.Vin[0];
+        // t > 0
+        for(let i=1; i<this.input.Vin.length; i++) {
+            this.t += this.dt;
+            this.Va = this.input.Vin[i]
+        }
+        this.cycleNumber++;
     }
+
+
 
 }
 
