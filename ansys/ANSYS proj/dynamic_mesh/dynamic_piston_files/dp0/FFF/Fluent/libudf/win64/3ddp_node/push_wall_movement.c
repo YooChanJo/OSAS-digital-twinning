42bba2eab1d02813d715326a482034830de39aec
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-12-14 16:21:24 +0900

    Added some pfailed but useful projects

---
+++ ansys/ANSYS proj/dynamic_mesh/dynamic_piston_files/dp0/FFF/Fluent/libudf/win64/3ddp_node/push_wall_movement.c
@@ -0,0 +1,20 @@
+#include "udf.h"
+#include "math.h"
+
+DEFINE_CG_MOTION(functionName, dt, vel, omega, time, dtime) {
+    /* Reset linear and angular velocities */
+    NV_S(vel, =, 0.0);
+    NV_S(omega, =, 0.0);
+
+    /* Define variables(constants) */
+    real amplitude = 0.02; // 20mm
+    real frequency = 1;
+    real angularVelocity = 2 * M_PI * frequency; // omega = 2 pi f
+
+    /* Compute velocities vel[1] is y velocity */
+    vel[1] = amplitude * angularVelocity * cos(angularVelocity * time); // A omega cos( omega t )
+    
+    /* Debugging */
+    printf("\nText Output");
+    printf("\ncurrent y-velocity : %g, time: %g\n", vel[1], time);
+}
